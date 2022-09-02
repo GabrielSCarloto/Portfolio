fd28bbd38e592c6edd205dc1cc8557cae170357d
GabrielSCarloto <carlotogabriel10@gmail.com>
2022-09-02 20:25:05 -0300

    Sobre Mim 2.0
    Falta o Scroll Reveal

--- js/script.js
+++ js/script.js
@@ -33,6 +33,12 @@ $(function() {
         $(".resumo-text").text("Olá, tudo bem? Eu sou o Gabriel");
         $(".developer-text").text("Desenvolvedor\nFull Stack Jr");
         $(".cv-text").text("Baixar CV");
+        $(".titulo-sobre-mim").text("SOBRE");
+        $(".titulo-blue").text("MIM");
+        $(".conteudo-texto1").text("Tenho 18 anos e sempre tive contato com tecnologia. Meus estudos na área começaram em 2021 e, tenho certeza que, continuarei estudando por um bom tempo para ser capaz de sempre entregar melhores resultados e inovações.");
+        $(".conteudo-texto2").text("Conclui o Ensino Médio no ano de 2021. Pretendo no futuro cursar Ciências da Computação ou Análise e Desenvolvimento de Sistemas.");
+        $(".conteudo-texto3").text("No começo de 2022 iniciei meus estudo no Instituto Proa, algo que me orgulho muito. Lá desenvolvi várias habilidades que você verá mais para baixo. Hoje, já conclui meu curso e estou estudando através da Udemy e tutoriais no Youtube.");
+        $(".conteudo-texto4").text("Falando um pouco sobre hobbies, os meus são: jogar basquete e futebol, assistir séries e podcasts, jogar games e escutar música.");
     });
 });
 
@@ -46,6 +52,12 @@ $(function() {
         $(".resumo-text").text("Hello, how are you? I’m Gabriel");
         $(".developer-text").text("Jr Full Stack\nDeveloper");
         $(".cv-text").text("Download CV");
+        $(".titulo-sobre-mim").text("ABOUT");
+        $(".titulo-blue").text("ME");
+        $(".conteudo-texto1").text("I am 18 years old and have always had contact with technology. My studies in the area started in 2021 and, I'm sure, I will continue studying for a long time to be able to always deliver better results and innovations.");
+        $(".conteudo-texto2").text("I finished high school in 2021. In the future I intend to study Computer Science or Systems Analysis and Development.");
+        $(".conteudo-texto3").text("At the beginning of 2022 I started my studies at Instituto Proa, I am very proud of this. In this period I developed several skills that you will see below. Today, I have completed my course and I am studying through Udemy and YouTube tutorials.");
+        $(".conteudo-texto4").text("Talking a little about hobbies, mine are: playing basketball and football, watching series and podcasts, playing games and listening to music.");
     });
 });
 
@@ -67,7 +79,11 @@ const stack1 = document.querySelector('.stack1');
 const stack2 = document.querySelector('.stack2');
 const stack3 = document.querySelector('.stack3');
 const cv = document.querySelector('.cv');
-const tituloSobreMim = document.querySelector(".titulo-sobre-mim")
+const tituloSobreMim = document.querySelector(".titulo-sobre-mim");
+const conteudoTexto1 = document.querySelector(".conteudo-texto1");
+const conteudoTexto2 = document.querySelector(".conteudo-texto2");
+const conteudoTexto3 = document.querySelector(".conteudo-texto3");
+const conteudoTexto4 = document.querySelector(".conteudo-texto4");
 
 btn.onclick = function() {
     this.classList.toggle('escuro');
@@ -87,4 +103,8 @@ btn.onclick = function() {
     stack3.classList.toggle('escuro');
     cv.classList.toggle('escuro');
     tituloSobreMim.classList.toggle('escuro');
+    conteudoTexto1.classList.toggle('escuro');
+    conteudoTexto2.classList.toggle('escuro');
+    conteudoTexto3.classList.toggle('escuro');
+    conteudoTexto4.classList.toggle('escuro');
 }
